7c0a10bdd374341b2109d6d08221fa970213c636
shindaeho <sdh077@naver.com>
2024-10-27 20:14:12 +0900

    contact

---
+++ src/app/(main)/contact/page.tsx
@@ -0,0 +1,12 @@
+import ContactForm from '@/widget/contact-form'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div className='container'>
+      <ContactForm />
+    </div>
+  )
+}
+
+export default Page
---
+++ src/widget/contact-form.tsx
@@ -0,0 +1,151 @@
+'use client'
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Textarea } from "@/components/ui/textarea"
+
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { z } from "zod"
+import { useForm } from "react-hook-form"
+import { useRouter } from "next/navigation"
+
+const FormSchema = z.object({
+  name: z.string().min(2, {
+    message: "name must be at least 2 characters.",
+  }),
+  phone: z.string().min(2, {
+    message: "phone must be at least 2 characters.",
+  }),
+  purpose: z.string().min(2, {
+    message: "purpose must be at least 2 characters.",
+  }),
+  description: z.string().min(2, {
+    message: "description must be at least 2 characters.",
+  })
+    .max(160, {
+      message: "Bio must not be longer than 30 characters.",
+    }),
+
+})
+
+const ContactForm = () => {
+  const router = useRouter()
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      name: "",
+      phone: "",
+      purpose: "",
+      description: "",
+    },
+  })
+  async function onSubmit(data: z.infer<typeof FormSchema>) {
+    try {
+      // FormData를 사용하여 데이터를 전송
+      const response = await fetch(`/api/contact`, {
+        method: 'POST',
+        body: JSON.stringify(data),
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to submit form');
+      }
+
+      const res = await response.json();
+      if (res.data) {
+        router.push(`/contact/${res.data.id}`)
+      }
+    } catch (error) {
+      console.error('Error submitting form:', error);
+    }
+  }
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-6 p-10 rounded-xl">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>성함</FormLabel>
+                <FormControl>
+                  <Input placeholder="이름을 입력해주세요" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="phone"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>연락처</FormLabel>
+                <FormControl>
+                  <Input type="" placeholder="연락처를 입력해주세요" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="purpose"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>목적</FormLabel>
+                <Select onValueChange={field.onChange} defaultValue={field.value}>
+                  <FormControl>
+                    <SelectTrigger className="w-full">
+                      <SelectValue placeholder="Select a purpose" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectLabel></SelectLabel>
+                      <SelectItem value="sample">샘플 신청</SelectItem>
+                      <SelectItem value="career">채용 문의</SelectItem>
+                      <SelectItem value="etc">기타 문의</SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>기타 문의</FormLabel>
+                <FormControl>
+                  <Textarea
+                    placeholder="전달 사항을 적어주세요"
+                    className="resize-none"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="max-w-40 ">Send message</Button>
+        </div>
+      </form>
+    </Form>
+  )
+}
+
+export default ContactForm
